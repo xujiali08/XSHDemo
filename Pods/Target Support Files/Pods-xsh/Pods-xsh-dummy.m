26137e05a82e78b5554434bf2b49c3b6eea3c242
jiali <jiali.xu@italki.com>
2020-12-30 17:20:44 +0800

    add pods

---
+++ Pods/Target Support Files/Pods-xsh/Pods-xsh-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xsh : NSObject
+@end
+@implementation PodsDummy_Pods_xsh
+@end
---
+++ Pods/Target Support Files/Pods-xshUITests/Pods-xshUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xshUITests : NSObject
+@end
+@implementation PodsDummy_Pods_xshUITests
+@end
